dbaf433c94c70a0bf5c055d70f187f8610cceb7f
Max Junior Soncco Mamani <msonccoma@unsa.edu.pe>
2026-04-12 21:48:33 -0500

    fix: agrega api para que sea el endpoint correcto

--- src/environment/context.js
+++ src/environment/context.js
@@ -1,2 +1,2 @@
 // Define la URL base para las solicitudes a la API
-export const API_BASE = 'https://sd-trabajo2-gupo1-back.onrender.com';
+export const API_BASE = 'https://sd-trabajo2-gupo1-back.onrender.com/api';
